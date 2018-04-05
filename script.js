5ea993db1ffea568983bc1190b54e7bf66925d9f
vikingviolinist <misopetrik@gmail.com>
2018-04-05 11:18:28 +0200

    Adds trigger animation on scroll

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-$(function(){
-	$('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width:$(this).attr('data-percent')
-		},6000);
-	});
+
+
+$(window).on('scroll', function() {
+	var skillsDiv = $('#skills');
+	var winT = $(window).scrollTop();
+	var winH = $(window).height();
+	var skillsT = skillsDiv.offset().top;
+	if (winT + winH > skillsT) {
+		$('.skillbar').each(function() {
+			$(this).find('.skillbar-bar').animate({
+				width:$(this).attr('data-percent')
+			}, 6000);
+		});
+	}
 });
